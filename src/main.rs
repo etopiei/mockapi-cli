cd6c1e2c8bf8c944dd46384c01f056cc4889cb96
etopiei <lj343@icloud.com>
2017-12-26 17:18:59 +1100

    Restructured args again so things are clearer

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,53 @@ fn main() {
         .author("etopiei lj343@icloud.com")
         .about("Creates API for testing from command line")
         .args_from_usage(
-            "-a, --action=[ACT] 'Specifies and action, start, stop or restart'
-             -p, --port=[PORT_NUMBER] 'Sets the port of the server'
-             -n, --new 'Creates new response for the server'
-             -c, --create 'Makes new server'
-             -d, --delete=[RESPONSE_NAME] 'Delete response'
-             -e, --edit=[RESPONSE_NAME] 'Edits a response'
-             -l, --list 'List response for given server'
-             -f, --file 'Create file for new response'
-             --editor 'Specify editor to change response'
-             -h, --help 'Show help message'
-             <servername> 'Sets the server to operate on'")
+            "-h, --help 'Show help message'
+            <servername> 'Current server'"
+        )
+        .subcommand(SubCommand::with_name("start")
+            .about("Start server")
+            .arg_from_usage(
+                "-p --port=[PORT_NUMBER] 'Set the port number'"
+            )
+        )
+        .subcommand(SubCommand::with_name("stop")
+            .about("Stop server")
+            .arg_from_usage(
+                "-f 'Force server to stop'"
+            )
+        )
+        .subcommand(SubCommand::with_name("restart")
+            .about("Restarts server")
+            .args_from_usage(
+                "-p --port=[PORT_NUMBER] 'Set the port number'"
+            )
+        )
+        .subcommand(SubCommand::with_name("delete")
+            .about("Deletes a response")
+            .args_from_usage(
+                "<response_name> 'Name of response to be deleted'"
+            )
+        )
+        .subcommand(SubCommand::with_name("create")
+            .about("Creates a new server")
+        )
+        .subcommand(SubCommand::with_name("new")
+            .about("Creates a new response for a server")
+            .args_from_usage(
+                "-t --type 'Sets the type, GET or POST (defualt is GET)'
+                 <response_name> 'Response Name'"
+            )
+        )
+        .subcommand(SubCommand::with_name("edit")
+            .about("Edits a server response")
+            .args_from_usage(
+                "--editor 'Sets the editor to edit the response (default is nano)'
+                 <response_name> 'Name of response to edit"
+            )
+        )
         .get_matches();
+
+        let servername = matches.value_of("servername").unwrap();
+        println!("Using server: {}", servername);
     
 }
